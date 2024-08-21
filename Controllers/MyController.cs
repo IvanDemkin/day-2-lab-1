915f18f9847e756b3f959983e2ad2f0692bcb8a1
Admin <Admin@DESKTOP-UBANCTC>
2024-08-21 15:15:51 +0700

    Add project files.

---
+++ Controllers/MyController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ControllersProj.Filters;
+
+namespace ControllersProj.Controllers
+{
+   // [MyActionFilter]
+    public class MyController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ContentResult ParamsObject(Models.Person p)
+        {
+            var c = new ContentResult();
+            c.Content = $" <h3>name: {p.Name} age: {p.Description}</h3>";
+            c.ContentType = "text/html";
+
+            return c;
+        }
+
+        [ActionName("Json")]
+        public IActionResult JsonAction(Models.Person p)
+        {            
+            return this.Json(p);
+        }
+
+        public IActionResult YaLink()
+        {
+            return this.Redirect("http://ya.ru");
+        }
+
+        public IActionResult GetFile()
+        {
+            string s = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "test.txt");
+            return PhysicalFile(s, "text/plain");
+        }
+
+       
+        public IActionResult somePage()
+        {
+            return View();
+        } 
+    }
+}
---
+++ Filters/MyActionFilterAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Text;
+using ControllersProj.Models;
+
+namespace ControllersProj.Filters
+{
+    public class MyActionFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            object? obj = context.ActionArguments.ElementAt(0).Value;
+            if (obj != null)
+            {
+                foreach (var prop in obj.GetType().GetProperties())
+                {
+                    sb.Append($"{prop.Name} {prop.GetValue(obj, null)}, ");
+                }
+            }
+            Console.WriteLine($"ActionExecuting. Params: {sb.ToString()}");
+        }
+    }
+}
